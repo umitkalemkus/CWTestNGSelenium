fe9b9ecdc5576dc69722d67f26021abe3841530a
umitkalemkus <umitkalemkus@yahoo.com>
2023-01-03 00:13:41 +0000

    02.01.2023

--- src/test/java/clarusway/tests/Day03_C06_CrossBrowser.java
+++ src/test/java/clarusway/tests/Day03_C06_CrossBrowser.java
@@ -1,16 +1,19 @@
 package clarusway.tests;
 
 import clarusway.utilities.BaseCrossBrowserTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Day03_C06_CrossBrowser extends BaseCrossBrowserTest {
 
-    // browser bilgisi testng xml dosyasindan alanacagindan dolayi kod asamasinda herhangi bir degisiklik goruntulenmeyecektir.
+    // browser bilgisi testng .xml dosyasindan alanacagindan dolayi kod asamasinda herhangi bir degisiklik goruntulenmeyecektir.
 
 
     @Test
-    public void crossBrowserTest(){
-
+    public void crossBrowserTest() throws InterruptedException {
+       driver.get("https://www.google.com/");
+       Thread.sleep(3000);
+        Assert.assertEquals(driver.getTitle(),"Google");
 
     }
 
