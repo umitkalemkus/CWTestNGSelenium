6cbe54204b3136abf4f393183e8f9846da6dc7d9
umitkalemkus <umitkalemkus@yahoo.com>
2023-01-04 09:27:16 +0000

    04.01.2023

---
+++ src/test/java/UmitPractise/ParalelTest_ParametreProjesi.java
@@ -0,0 +1,22 @@
+package UmitPractise;
+
+import clarusway.utilities.Basetest;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ParalelTest_ParametreProjesi extends Basetest {
+
+    // http://automationpractice.com/index.php
+
+    @Test
+    public void test(){
+        Actions action = new Actions(driver);
+        driver.get("https://www.ayakkabidunyasi.com.tr/");
+
+
+
+
+
+
+    }
+}
