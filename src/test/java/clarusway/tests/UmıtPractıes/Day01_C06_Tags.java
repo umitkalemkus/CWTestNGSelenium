45cb52c2d7f8e05f3b85c4020d541e02dc27cec2
umitkalemkus <umitkalemkus@yahoo.com>
2023-03-17 12:09:10 +0000

    done

--- src/test/java/clarusway/tests/UmıtPractıes/Day01_C06_Tags.java
+++ src/test/java/clarusway/tests/UmıtPractıes/Day01_C06_Tags.java
@@ -4,7 +4,12 @@ import org.testng.annotations.Test;
 
 public class Day01_C06_Tags {
 
+    public static void main(String[] args) {
+
+        System.out.println("args = " + args);
+        
+        
+    }
 
 
-    @Test
 }
